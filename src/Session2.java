140e5ebaeefd7e6f34d5cd0f6c1fb32283369b2a
PhaniMKMS <fullstackcodecrush@gmail.com>
2025-01-17 15:09:19 +0530

    Session 2- conditional statements example

---
+++ src/Session2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Session2 {
+
+    // if, else, if-else, else-if , nested if - else  (conditional statements)
+    //  for , while , do-while (loops)
+
+    public static void main(String[] args) {
+//        Scanner input=new Scanner(System.in);
+//        System.out.println("Enter person's age");
+//        int age=input.nextInt();
+//        System.out.println("Enter person's name");
+//        String name=input.next();
+//        if(age>=18){
+//            System.out.println(name + " is a major");
+//        }else{
+//            System.out.println(name+ " is a minor");
+//        }
+//
+
+//int age = 28;  // for minors 20 % discount
+//boolean isEligible=false; // 30% discount special discount
+//int price=100;
+//double finalPrice;
+//        if(age>18){
+//            if(isEligible){
+//             finalPrice=price * 0.7;
+//
+//            }else{
+//                finalPrice=price;
+//            }
+//        }else{
+//            //eligible for 20% discount
+//            if(isEligible){
+//                //additional 30% discount
+//                finalPrice=price * 0.5;
+//
+//            }else{
+//                finalPrice=price* 0.8;
+//            }
+//        }
+//
+//        System.out.println(" Final price :: "+finalPrice);
+//
+ }
+}
